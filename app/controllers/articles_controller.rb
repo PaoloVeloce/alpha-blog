9a20e59099e95ccd0655378583ef648a0e6da510
Paolo Veloce <Pablo.veloce@gmail.com>
2016-01-16 16:04:27 +0000

    style to errors and partial. Add _errors partial in shared folder. Some changes in article controller, about :success green and :danger red removing :notice from flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created!"
+      flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "The article was successfully updated!"
+      flash[:success] = "The article was successfully updated!"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -40,7 +40,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "Your article was successfully deleted!"
+    flash[:danger] = "Your article was successfully deleted!"
     redirect_to articles_path
   end
   
