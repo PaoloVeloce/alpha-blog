1b61c7b7d9e446a01b693c59c908f21819be80fc
Paolo Veloce <Pablo.veloce@gmail.com>
2016-02-05 10:06:33 +0000

    finished authentication system with addition iof restriction on UI and controllers levels

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:update, :edit, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  
   
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
@@ -47,6 +49,13 @@ class UsersController < ApplicationController
   
   def set_user
     @user = User.find(params[:id]) 
+  end
+  
+  def require_same_user
+    if current_user != @user
+    flash[:danger] = "You can edit only yor own account"
+    redirect_to root_path
+    end
   end
 
 end
